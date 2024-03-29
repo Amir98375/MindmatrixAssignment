c8145629205fe515d15a14c9091c1b9f0858dcf2
amir <mohd.a@test.com>
2024-03-29 18:31:35 +0530

    some minor changes

--- src/TaskManager/Taskmanager.jsx
+++ src/TaskManager/Taskmanager.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css'
 import Swal from 'sweetalert2';
 import { MdDelete } from "react-icons/md";
@@ -8,35 +8,81 @@ function TaskManager() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
   const [assignee, setAssignee] = useState('');
-
+  const [todoList,setTodoList]=useState({})
+  const [selectedUser,setSelectedUser]=useState('')
   const handleAddTask = () => {
-    if(assignee){
-        if (newTask.trim() !== '') {
-            setTasks([
-              ...tasks,
-              {
-                id: tasks.length + 1,
-                title: newTask,
-                assignee: assignee || 'Unassigned'
-              }
-            ]);
-            setNewTask('');
-            setAssignee('');
-          }
-    }else{
-        Swal.fire({
-            // title: "Enter Name Of Assignee",
-            text: "Enter Assignee Name",
-            icon: "error"
-          });
+    if (assignee && newTask.trim() !== '') {
+      setTodoList(prevTodoList => {
+        const updatedTodoList = { ...prevTodoList };
+        if (updatedTodoList.hasOwnProperty(assignee)) {
+          updatedTodoList[assignee] = updatedTodoList[assignee].concat(newTask);
+        } else {
+          updatedTodoList[assignee] = [newTask];
+        }
+        return updatedTodoList;
+      });
+      setNewTask('');
+      setAssignee('');
+    } else {
+      Swal.fire({
+        text: "Please enter both assignee name and task",
+        icon: "error"
+      });
     }
-
   };
+  
+  // const handleAddTask = () => {
+  //   if(assignee){
+      
+  //     setTodoList(prevTodoList => {
+  //       const updatedTodoList = { ...prevTodoList };
+  //       if (updatedTodoList.hasOwnProperty(assignee)) {
+  //         updatedTodoList[assignee] = updatedTodoList[assignee].concat(newTask);
+  //       } else {
+  //         updatedTodoList[assignee] = newTask;
+  //       }
+  //       return updatedTodoList;
+  //     });
+  //               setNewTask('');
+  //           setAssignee('');
+      
+  //       // if (newTask.trim() !== '') {
+  //       //     setTasks([
+  //       //       ...tasks,
+  //       //       {
+  //       //         id: tasks.length + 1,
+  //       //         title: newTask,
+  //       //         assignee: assignee || 'Unassigned'
+  //       //       }
+  //       //     ]);
+  //       //     setNewTask('');
+  //       //     setAssignee('');
+  //       //   }
+  //   }else{
+  //       Swal.fire({
+  //           // title: "Enter Name Of Assignee",
+  //           text: "Enter Assignee Name",
+  //           icon: "error"
+  //         });
+  //   }
 
+  // };
+  console.log(todoList,'todoList')
+  useEffect(() => {
+    if (selectedUser) {
+      const filteredTasks = todoList[selectedUser] || [];
+      setTasks(filteredTasks);
+    } else {
+      setTasks([]);
+    }
+  }, [selectedUser, todoList]);
+function setDataToTable(){
+  let filterArr=todoList.filter()
+}
   const handleDeleteTask = id => {
     setTasks(tasks.filter(task => task.id !== id));
   };
-
+console.log(tasks,"my task filtered")
   return (
     <div>
       <h1>Task Manager</h1>
@@ -68,6 +114,15 @@ function TaskManager() {
       </div>
       <br></br>
       <button onClick={handleAddTask} className='button'>Add Task</button>
+      <br /><br />
+      <select value={selectedUser} onChange={e => setSelectedUser(e.target.value)}>
+          <option value="">Select Assignee</option>
+          {Object.keys(todoList).map((user, index) => (
+            <option key={index} value={user}>{user}</option>
+          ))}
+          {/* Add more options as needed */}
+        </select>
+
       <div>
         {/* <h2>Tasks:</h2>
         <ul>
@@ -107,7 +162,10 @@ function TaskManager() {
             padding: 0,
           },
         }}
-        data={tasks}
+        // data={tasks}
+        // data={tasks.map((task, index) => ({ task, assignee: selectedUser }))}
+        data={tasks.map((task, index) => ({ task, assignee: selectedUser }))}
+        // data={filteredTasks.map((task, index) => ({ task, assignee }))}
         actions={
           [
             // {
